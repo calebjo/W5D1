a2387e0ff52d6143043de7970eb209b219fd443c
calebjo <caleb.ri.jones@gmail.com>
2021-11-01 13:56:56 -0400

    Add methods for 00_tree_node.rb

--- map_lru/lib/00_tree_node.rb
+++ map_lru/lib/00_tree_node.rb
@@ -1,3 +1,30 @@
 class PolyTreeNode
-    
+    #include Searchable
+    attr_reader :value, :parent, :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)
+        @parent = node
+        
+    end
+
+    def add_child(node)
+        if !@children.include?(node)
+            @children << node
+            node.parent = self
+        end
+    end
+
+    def remove_child(node)
+        if !@children.include?(node)
+            raise 'Node is not a child.'
+        end
+        node.parent = nil
+        @children.delete(node)
+    end
 end
