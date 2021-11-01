25161be4637cfeec1c5217b3ca6b46d878ce587f
tc8appdevelo <tc8appdevelo@gmail.com>
2021-11-01 13:21:21 -0500

    fixed parent=, and bfs

--- map_lru/lib/00_tree_node.rb
+++ map_lru/lib/00_tree_node.rb
@@ -1,5 +1,19 @@
+module Searchable
+
+    def bfs(root, target)
+        queue = [root]
+        until queue.empty?
+            this_node = queue.shift
+            return this_node.value if this_node.value == target
+            this_node.children.each { |child| queue << child }
+        end
+        nil
+    end
+    
+end
+
 class PolyTreeNode
-    #include Searchable
+    include Searchable
     attr_reader :value, :parent, :children
 
     def initialize(value)
@@ -9,8 +23,13 @@ class PolyTreeNode
     end
 
     def parent=(node)
+        if !@parent.nil?
+            @parent.children.delete(self)
+        end
         @parent = node
-        
+        if !node.nil? && !node.children.include?(self)
+            node.children << self
+        end
     end
 
     def add_child(node)
@@ -22,9 +41,11 @@ class PolyTreeNode
 
     def remove_child(node)
         if !@children.include?(node)
-            raise 'Node is not a child.'
+            raise ArgumentError.new('Node is not a child.')
         end
         node.parent = nil
         @children.delete(node)
     end
 end
+
+
