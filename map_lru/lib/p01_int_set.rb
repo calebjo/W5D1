6cb4c66aba0e299a8799c0bf27ac2f28cefdafc7
tc8appdevelo <tc8appdevelo@gmail.com>
2021-11-01 15:24:21 -0500

    finished p01_int_set

--- map_lru/lib/p01_int_set.rb
+++ map_lru/lib/p01_int_set.rb
@@ -40,10 +40,11 @@ class IntSet
   end
 
   def insert(num)
-
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
@@ -74,24 +75,43 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if @count >= num_buckets
+    if !self[num].include?(num)
+      self[num] << num
+      @count += 1 
+    end
+
   end
 
   def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    @store.each do |bucket|
+      return true if bucket.include?(num)
+    end
+    false
   end
+
+
 
   private
-
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
+
 
   def num_buckets
     @store.length
   end
 
   def resize!
+    new_buckets = Array.new(num_buckets) { Array.new }
+    @store += new_buckets
   end
 end
